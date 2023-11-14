4ea748cf092b3fa5724f4f67159e8cb31c729562
Reallynotanalien <kafortin@student.42quebec.com>
2023-11-14 14:40:08 -0500

    K 11-14: Exec works execpt for cat

--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -13,17 +13,42 @@ int	count_commands(t_command *cmd)
 	return (i);
 }
 
-void	child(t_command **cmd, char **env)
+void	child_two(t_command **cmd)
+{
+	if (pipe(use_data()->fd) < 0)
+		printf("PIPE did not work\n");
+	use_data()->pid = fork();
+	if (use_data()->pid == -1)
+		printf("FORK did not work\n");
+	else if (use_data()->pid == 0)
+	{
+		close(use_data()->fd[0]);
+		close(use_data()->fd[1]);
+		if ((*cmd)->outfile != STDOUT_FILENO)
+		{
+			dup2((*cmd)->outfile, STDOUT_FILENO);
+			close((*cmd)->outfile);
+		}
+		execve((*cmd)->path, (*cmd)->cmd, use_data()->new_env);
+	}
+	else
+	{
+		close(use_data()->fd[0]);
+		close(use_data()->fd[1]);
+		waitpid(use_data()->pid, NULL, 0);
+	}
+}
+
+void	child_one(t_command **cmd)
 {
 	close(use_data()->fd[0]);
 	close((*cmd)->outfile);
 	dup2(use_data()->fd[1], STDOUT_FILENO);
 	close(use_data()->fd[1]);
-	execve((*cmd)->path, (*cmd)->cmd, env);
-	printf("execve de child has happened\n");
+	execve((*cmd)->path, (*cmd)->cmd, use_data()->new_env);
 }
 
-void	pipex(t_command **cmd, char **env)
+void	pipex(t_command **cmd)
 {
 	if ((*cmd)->infile != STDIN_FILENO)
 	{
@@ -36,16 +61,11 @@ void	pipex(t_command **cmd, char **env)
 	if (use_data()->pid == -1)
 		printf("FORK did not work\n");
 	else if (use_data()->pid == 0)
-	{
-		child(cmd, env);
-		printf("my pid is %i\n", use_data()->pid);
-	}
+		child_one(cmd);
 	else
 	{
-		printf("my pid is %i\n", use_data()->pid);
 		close(use_data()->fd[1]);
 		(*cmd)->next->infile = use_data()->fd[0];
-		// dup2(use_data()->fd[0], STDIN_FILENO);
 		close(use_data()->fd[0]);
 	}
 }
@@ -55,38 +75,14 @@ void	exec(t_command *cmd)
 	int	nb_cmds;
 
 	nb_cmds = count_commands(cmd);
-	
 	while (cmd && nb_cmds > 1)
 	{
 		get_path(cmd);
-		pipex(&cmd, use_data()->new_env);
+		pipex(&cmd);
 		nb_cmds--;
 		if (cmd->next)
 			cmd = cmd->next;
-		printf("nb cmds actuel: %d\n", nb_cmds);
 	}
 	get_path(cmd);
-	if (pipe(use_data()->fd) < 0)
-		printf("PIPE did not work\n");
-	use_data()->pid = fork();
-	if (use_data()->pid == -1)
-		printf("FORK did not work\n");
-	else if (use_data()->pid == 0)
-	{
-		close(use_data()->fd[0]);
-		close(use_data()->fd[1]);
-		if (cmd->outfile != STDOUT_FILENO)
-		{
-			dup2(cmd->outfile, STDOUT_FILENO);
-			close(cmd->outfile);
-		}
-		execve(cmd->path, cmd->cmd, use_data()->new_env);
-		printf("execve de la fin has happened\n");
-	}
-	else
-	{
-		close(use_data()->fd[0]);
-		close(use_data()->fd[1]);
-		waitpid(use_data()->pid, NULL, 0);
-	}
+	child_two(&cmd);
 }
