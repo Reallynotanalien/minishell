b7a03f6333ecfae47ea3f56394d6adffef598b5d
Reallynotanalien <kafortin@student.42quebec.com>
2023-11-14 14:32:35 -0500

    K 11-14: EXEC LOOPS SEEMS TO BE WORKING WELL (exept for cat)

--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -13,47 +13,39 @@ int	count_commands(t_command *cmd)
 	return (i);
 }
 
-void	execute_one_command(t_command **cmd, char **env)
-{
-	use_data()->pid = fork();
-	if (use_data()->pid == -1)
-		printf("FORK did not work\n");
-	else if (use_data()->pid == 0)
-	{
-		if ((*cmd)->outfile != STDOUT_FILENO)
-		{
-			dup2((*cmd)->outfile, STDOUT_FILENO);
-			close((*cmd)->outfile);
-		}
-		execve((*cmd)->path, (char *const *)(*cmd)->cmd, env);
-		// exit(0);
-	}
-	waitpid(use_data()->pid, NULL, 0);
-}
-
 void	child(t_command **cmd, char **env)
 {
 	close(use_data()->fd[0]);
 	close((*cmd)->outfile);
 	dup2(use_data()->fd[1], STDOUT_FILENO);
 	close(use_data()->fd[1]);
-	execve((*cmd)->path, (char *const *)(*cmd)->cmd, env);
-	// exit(0);
+	execve((*cmd)->path, (*cmd)->cmd, env);
+	printf("execve de child has happened\n");
 }
 
 void	pipex(t_command **cmd, char **env)
 {
+	if ((*cmd)->infile != STDIN_FILENO)
+	{
+		dup2((*cmd)->infile, STDIN_FILENO);
+		close((*cmd)->infile);
+	}
 	if (pipe(use_data()->fd) < 0)
 		printf("PIPE did not work\n");
 	use_data()->pid = fork();
 	if (use_data()->pid == -1)
 		printf("FORK did not work\n");
 	else if (use_data()->pid == 0)
+	{
 		child(cmd, env);
+		printf("my pid is %i\n", use_data()->pid);
+	}
 	else
 	{
+		printf("my pid is %i\n", use_data()->pid);
 		close(use_data()->fd[1]);
-		dup2(use_data()->fd[0], STDIN_FILENO);
+		(*cmd)->next->infile = use_data()->fd[0];
+		// dup2(use_data()->fd[0], STDIN_FILENO);
 		close(use_data()->fd[0]);
 	}
 }
@@ -63,43 +55,36 @@ void	exec(t_command *cmd)
 	int	nb_cmds;
 
 	nb_cmds = count_commands(cmd);
-	if (cmd->infile != STDIN_FILENO)
-	{
-		dup2(cmd->infile, STDIN_FILENO);
-		close(cmd->infile);
-	}
-	if (nb_cmds == 1)
+	
+	while (cmd && nb_cmds > 1)
 	{
 		get_path(cmd);
-		execute_one_command(&cmd, use_data()->new_env);
+		pipex(&cmd, use_data()->new_env);
+		nb_cmds--;
+		if (cmd->next)
+			cmd = cmd->next;
+		printf("nb cmds actuel: %d\n", nb_cmds);
+	}
+	get_path(cmd);
+	if (pipe(use_data()->fd) < 0)
+		printf("PIPE did not work\n");
+	use_data()->pid = fork();
+	if (use_data()->pid == -1)
+		printf("FORK did not work\n");
+	else if (use_data()->pid == 0)
+	{
+		close(use_data()->fd[0]);
+		close(use_data()->fd[1]);
+		if (cmd->outfile != STDOUT_FILENO)
+		{
+			dup2(cmd->outfile, STDOUT_FILENO);
+			close(cmd->outfile);
+		}
+		execve(cmd->path, cmd->cmd, use_data()->new_env);
+		printf("execve de la fin has happened\n");
 	}
 	else
 	{
-		while (cmd && nb_cmds > 1)
-		{
-			get_path(cmd);
-			pipex(&cmd, use_data()->new_env);
-			nb_cmds--;
-			if (cmd->next)
-				cmd = cmd->next;
-		}
-		get_path(cmd);
-		if (pipe(use_data()->fd) < 0)
-			printf("PIPE did not work\n");
-		use_data()->pid = fork();
-		if (use_data()->pid == -1)
-			printf("FORK did not work\n");
-		else if (use_data()->pid == 0)
-		{
-			close(use_data()->fd[0]);
-			close(use_data()->fd[1]);
-			if (cmd->outfile != STDOUT_FILENO)
-			{
-				dup2(cmd->outfile, STDOUT_FILENO);
-				close(cmd->outfile);
-			}
-			execve(cmd->path, (char *const *)cmd->cmd, use_data()->new_env);
-		}
 		close(use_data()->fd[0]);
 		close(use_data()->fd[1]);
 		waitpid(use_data()->pid, NULL, 0);
